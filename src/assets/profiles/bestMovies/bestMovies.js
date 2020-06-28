7fbe1599c28582905f8b3fc454f2996924dec339
Qingtian Mei <39237932+frankmei1@users.noreply.github.com>
2020-06-28 17:13:50 -0400

    push an image

--- src/assets/profiles/bestMovies/bestMovies.js
+++ src/assets/profiles/bestMovies/bestMovies.js
@@ -29,6 +29,6 @@ export default
     "id": 5,
     "name": "Berserk ",
     "time": "1997",
-    "img": {"src": "challenges/media/pushup.png", "alt" : "" }
+    "img": {"src": "https://github.com/frankmei1/MyPersonalApp/blob/master/src/assets/profiles/bestMovies/Berserk_The_Golden_Age.jpg?raw=true", "alt" : "" }
   },
 ]
